e9643056f73f9c0140f3b4a2242e8b1616717f1f
Shinji Yamashita <syamas@uw.edu>
2020-11-07 19:05:35 +0800

    just need to host webpage

--- analysis.R
+++ analysis.R
@@ -1,12 +1,14 @@
 
 
 
+
+
 best_50_2019 <- read.csv("data/top50.csv", stringsAsFactors = FALSE)
 
 # These were the top 50 tracks in spotify from 2019
 
-library("dplyr")  
-library("stringr")
+
+
 
 
 top_3_popularity <- best_50_2019 %>%
@@ -18,33 +20,33 @@ pop_genre <- best_50_2019 %>%
   filter(Genre == "pop") 
 
 
-max_pop_track <- best_50_2019 %>%
+max_energy_poptrack <- best_50_2019 %>%
   arrange(-Energy) %>%
   top_n(5) %>%
   filter(Energy == max(Energy, na.rm = TRUE)) %>%
   pull(Track.Name)
+
   
   
   
-Billie_most_popular <- best_50_2019 %>%
+Billie_most_popular_num <- best_50_2019 %>%
   filter(str_detect(Artist.Name, "Billie Eilish")) %>%
   filter(Popularity == max(Popularity, na.rm = TRUE)) %>%
   pull(Popularity)
-  
-  
-rm(max_total_set_artist)
-  
+
+most_favorite_artist <- best_50_2019 %>%
+  filter(Artist.Name == "Lil Tecca") %>%
+  pull(Artist.Name)
+
+
+favorite_track <- best_50_2019$Track.Name[best_50_2019$Track.Name == "Ransom"]
+ 
+
+
   
 most_top50_songs_artist <- tail(names(sort(table(best_50_2019$Artist.Name))), 1)
 
-rm(most_top50_song)
-
-
-
-
-  
-library("stringr")
-
+library("ggplot2")
 
 
 
